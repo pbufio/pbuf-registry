7546b21cd245b7bea72fa72305519e48f14d9c93
Dmitry Ulyanov <dmitryi.ulianov@gmail.com>
2024-02-05 18:38:11 +0300

    token server mock methods

--- internal/server/token.go
+++ internal/server/token.go
@@ -1,16 +1,19 @@
 package server
 
 import (
+	"context"
+
 	"github.com/go-kratos/kratos/v2/log"
 	v1 "github.com/pbufio/pbuf-registry/gen/pbuf-registry/v1"
 	"github.com/pbufio/pbuf-registry/internal/data"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type TokenServer struct {
 	v1.UnimplementedTokenServiceServer
-	tokenRepository    data.TokenRepository
-	metadataRepository data.MetadataRepository
-	logger             *log.Helper
+	tokenRepository data.TokenRepository
+	logger          *log.Helper
 }
 
 func NewTokenServer(
@@ -21,4 +24,11 @@ func NewTokenServer(
 		tokenRepository: tokenRepository,
 		logger:          log.NewHelper(log.With(logger, "module", "server/TokenServer")),
 	}
+}
+
+func (TokenServer) RegisterToken(context.Context, *v1.RegisterTokenRequest) (*v1.RegisterTokenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RegisterToken not implemented")
+}
+func (TokenServer) RevokeToken(context.Context, *v1.RevokeTokenRequest) (*v1.RevokeTokenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RevokeToken not implemented")
 }
